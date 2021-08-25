fdb30a4d0ae02a6a9f1c29a2a1db1b2c121c486b
Patrick <patrickkottwitz@gmail.com>
2021-08-25 14:21:54 -0300

    estrutura inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types'
+import Head from 'next/head'
+// ThemeProvider para utilizar o theme.js
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+// importa o tema
+import theme from '../src/theme'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp(props) {
+  const { Component, pageProps } = props // Component e pageProps as propriedades da pagina
+
+  return (
+    <>
+      {/* import Head */}
+      <Head>
+        <title>Anunx</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      {/* injetando o tema */}
+      <ThemeProvider theme={theme}>
+        {/* Css */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+
+
 }
 
-export default MyApp
+
+// import PropType
+MyApp.propTypes = {
+  Componente: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+}
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from '@material-ui/core/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000000',
+    }
+  }
+})
