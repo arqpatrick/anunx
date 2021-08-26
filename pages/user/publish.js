dff831a2c8a851ce9ddfd02ef9c6a6341cfac7d2
Patrick <patrickkottwitz@gmail.com>
2021-08-26 00:09:36 -0300

    configuração do campo de imagem

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -1,10 +1,22 @@
-import { Box, Button, Container, Select, TextField, Typography } from '@material-ui/core'
+import { 
+  Box, 
+  Button, 
+  Container, 
+  IconButton, 
+  Select, 
+  TextField, 
+  Typography 
+} from '@material-ui/core'
+
 import { makeStyles } from '@material-ui/core/styles'
+import { DeleteForever } from '@material-ui/icons'
 
 import TemplateDefault from '../../src/templates/Default'
 
 // CSS
 const useStyles = makeStyles((theme) => ({
+  mask: {},
+  mainImage: {},
   container: {
     padding: theme.spacing(8, 0, 6)
   },
@@ -14,7 +26,52 @@ const useStyles = makeStyles((theme) => ({
   box: {
     backgroundColor: theme.palette.background.white,
     padding: theme.spacing(3),
-  }
+  },
+  thumbsContainer: {
+    display: 'flex',
+    marginTop: 15,
+  },
+  dropzone: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 10,
+    width: 200,
+    height: 150,
+    margin: '0 15px 15px 0',
+    backgroundColor: theme.palette.background.default,
+    border: '2px dashed black'
+  },
+  thumb: {
+    position: 'relative',
+    width: 200,
+    height: 150,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+
+    '& $mainImage': {
+      backgroundColor: 'green',
+      padding: '6px 10px',
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+    },
+
+    '&:hover $mask': {
+      display: 'flex',
+    },
+
+    '& $mask': {
+      display: 'none',
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      width: '100%',
+      height: '100%',
+    }
+  },
 }))
 
 
@@ -86,6 +143,31 @@ const Publish = () => {
           <Typography component="div" variant="body2" color="textPrimary">
             A primeira imagem é a foto principal do seu anúncio.
           </Typography>
+          <Box className={classes.thumbsContainer}>
+            <Box className={classes.dropzone}>
+              <Typography variant="body2" color="textPrimary">
+                Clique para adicionar ou arraste a imagem aqui.
+              </Typography>
+            </Box>
+
+            <Box 
+              className={classes.thumb}
+              style={{ backgroundImage: 'url(https://source.unsplash.com/random)'}}
+            >
+              <Box className={classes.mainImage}>
+                <Typography variant="body2" color="secondary">
+                  Principal
+                </Typography>
+              </Box>
+
+              <Box className={classes.mask}>
+                <IconButton color="secondary">
+                  <DeleteForever fontSize="large" />
+                </IconButton>
+              </Box>
+            </Box>
+
+          </Box>
         </Box>
       </Container>
 
